5b3761c92b1b6cb481a114cea339df4328ae13f3
Sean <56064050+seangoats@users.noreply.github.com>
2022-01-07 16:38:55 +0800

    Fix type

--- pages/api/hawker_centre/[id].ts
+++ pages/api/hawker_centre/[id].ts
@@ -6,10 +6,10 @@ import Cors from 'cors';
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function initMiddleware(middleware) {
+function initMiddleware(middleware: any) {
 	return (req: NextApiRequest, res: NextApiResponse) =>
 		new Promise((resolve, reject) => {
-			middleware(req, res, (result) => {
+			middleware(req, res, (result: any) => {
 				if (result instanceof Error) {
 					return reject(result);
 				}
