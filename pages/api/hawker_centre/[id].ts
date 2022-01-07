e48fbd5c597eaf26ca22cfe78d66f3b802a854a3
Sean <56064050+seangoats@users.noreply.github.com>
2022-01-07 16:35:46 +0800

    Add cors

--- pages/api/hawker_centre/[id].ts
+++ pages/api/hawker_centre/[id].ts
@@ -2,8 +2,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import { HawkerAPIRecord } from '@interfaces/hawkerCentre';
+import Cors from 'cors';
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function initMiddleware(middleware) {
+	return (req: NextApiRequest, res: NextApiResponse) =>
+		new Promise((resolve, reject) => {
+			middleware(req, res, (result) => {
+				if (result instanceof Error) {
+					return reject(result);
+				}
+				return resolve(result);
+			});
+		});
+}
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+	// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+	Cors({
+		// Only allow requests with GET, POST and OPTIONS
+		methods: ['GET', 'POST', 'OPTIONS'],
+	})
+);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<HawkerAPIRecord>) {
+	// Run cors
+	await cors(req, res);
+
 	const {
 		query: { id },
 		method,
